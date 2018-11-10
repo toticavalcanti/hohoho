f3dfe92052f15497f8a09556704f13d05af650e7
toti.cavalcanti <toticavalcanti@hotmail.com>
2018-11-10 16:48:42 -0200

    create a second test with 2 as parameter and expected Ho Ho! as return

--- test_noel.py
+++ test_noel.py
@@ -9,4 +9,7 @@ class TestNoel(unittest.TestCase):
     def test_one_Ho(self):
         self.assertEqual(solution(1), "Ho!")
 
+    def test_two_Ho(self):
+        self.assertEqual(solution(2), "Ho Ho!")
+
 unittest.main()
